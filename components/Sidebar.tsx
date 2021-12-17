d4ed6179c33dad7e0be7a0cc7874d0026d362989
Kelvin Brito <britok30@gmail.com>
2021-12-17 16:24:31 -0500

    add typescript

---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Sidebar = () => {
+    return (
+        <div>
+            <h1>Sidebar component</h1>
+        </div>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Home() {
     return (
@@ -11,7 +12,7 @@ export default function Home() {
             <h1>Spotify</h1>
 
             <main className="flex">
-                {/* sidebar */}
+                <Sidebar />
                 {/* center */}
             </main>
             <div>{/* player */}</div>
