7d36c2607de641937ee691eee62e970c14f61934
Kelvin Brito <britok30@gmail.com>
2021-12-26 13:47:18 -0500

    middleware change

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -6,14 +6,17 @@ export const middleware = async (req: any) => {
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
     const { pathname } = req.nextUrl;
 
-    // Allow the requests if the following is true...
-    // 1) Its a request for next-auth session & provider fetching
-    // 2) the token exists
+    // if user is already signedin, but goes to login page, redirect to home page
+    if (token && pathname === '/login') {
+        return NextResponse.redirect('/');
+    }
 
+    //if user wants to sign in
     if (pathname.includes('/api/auth') || token) {
         return NextResponse.next();
     }
 
+    //redirect to login if there is no token, and are requesting a protected route
     if (!token && pathname !== '/login') {
         return NextResponse.redirect('/login');
     }
